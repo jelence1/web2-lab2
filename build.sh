9a644024e6f144c4fe538ea25c6adcd69862fec6
jelence1 <jelena.gavran@fer.hr>
2022-11-03 16:48:41 +0100

    lab2

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py flush --no-input
-
 python manage.py collectstatic --no-input
 
+python manage.py makemigrations
+python manage.py migrate
+
 python -Xutf8 manage.py loaddata data.json
+
 python manage.py makemigrations
 python manage.py migrate
