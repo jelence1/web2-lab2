ceda8e4111dae68b8a954435d4a5388d9bd7d806
jelence1 <jelena.gavran@fer.hr>
2022-11-03 17:09:06 +0100

    a

--- lab2_Project/settings.py
+++ lab2_Project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "web2-lab2-6ri3.onrender.com"]
 
 
 # Application definition
